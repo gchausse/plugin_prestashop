134fa28118d4a9543df2d0e09b13ae1712b1f1ac
guillaume <guillaume.chausse@live.fr>
2017-07-05 09:21:49 +0200

    initial commit

---
+++ modules/mymodule/classes/MyModule.php
@@ -0,0 +1,36 @@
+<?php
+class MyModule extends Module{
+	public function __construct(){
+	    $this->name = 'mymodule';
+	    $this->tab = 'front_office_features';
+	    $this->version = '1.0.0';
+	    $this->author = 'Guillaume Chausse \'THE BEST\'';
+	    $this->need_instance = 0;
+	    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+	    $this->bootstrap = true;
+
+	    parent::__construct();
+
+	    $this->displayName = $this->l('My module');
+	    $this->description = $this->l('Module qui ne sert pas a grand chose');
+
+	    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+	    if (!Configuration::get('MYMODULE_NAME'))
+	       $this->warning = $this->l('No name provided');
+	}
+
+	public function install(){
+	  if (!parent::install())
+	    return false;
+	  return true;
+	}
+
+	public function uninstall(){
+	  if (!parent::uninstall())
+	    return false;
+	  return true;
+	}
+
+} 
+?>
---
+++ modules/mymodule/mymodule.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('_PS_VERSION_')){
+    exit;
+}
+require _PS_MODULE_DIR_."mymodule/classes/MyModule.php";
+?>
